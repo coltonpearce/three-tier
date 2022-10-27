7f0f907aff38b63922f25aa6dfd822000dab2c1b
coltonpearce <cpearce3@byu.edu>
2022-10-27 13:31:27 -0600

    IS401Commit

--- datagrid.js
+++ datagrid.js
@@ -1,6 +1,7 @@
 // URL to your production code when you publish yoru google app script as a web app
 // You will get this URL when you publish your google apps script as a web app
-let prefix="YourWebAppURLHere" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
+let prefix="https://script.google.com/macros/s/AKfycbyrWAxh-z868nNgv44YCUhOWhMEwms3fvDslXZP0nB_bcbGhToDfPy07CDPFO_Hkq1K/exec" 
+// looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
 const dealy_seconds = 4 // how long data validation messages are visible
 
 //example call: file:///C:/Users/Gove/three-tier/datagrid.html?employee
